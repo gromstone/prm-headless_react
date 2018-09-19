c67336dbf949a30ee1f5e71b226441863aa81a9e
David R <drodriguez@paymentus.com>
2018-09-19 12:29:01 -0400

    scss adjustment

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
-import './index.scss';
+import './scss/index.scss';
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
